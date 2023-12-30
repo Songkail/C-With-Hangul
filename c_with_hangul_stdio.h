866f2a37b5c1b0b5e3f5fdc2f0170bbab49d6b34
Songkail <123148747+Songkail@users.noreply.github.com>
2023-12-31 03:36:01 +0900

    Add files via upload

---
+++ c_with_hangul_stdio.h
@@ -0,0 +1,24 @@
+/* Made on December 31, 2023 by Songkail */
+
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stdio.h>
+
+#define 출력 printf
+#define 입력 scanf
+
+#define 만일 if
+#define 대해 for
+#define 동안 while
+
+#define 주요 main
+
+#define 반환 return
+
+typedef int 정수;
+typedef float 실수;
+typedef char 문자;
+typedef void 공허;
+
+#endif
